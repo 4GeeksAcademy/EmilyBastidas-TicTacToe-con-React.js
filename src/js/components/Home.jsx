b0f9c7dfe86d5ed6d97cd519ec2a4810140d46fe
EBastidas <114364709+EmilyBastidas@users.noreply.github.com>
2025-11-12 01:21:08 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+//componentes
+import Index from './Index';
+import Game from './Game';
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const Home = () => {
+	// Empezamos mostrando 'Index'
+	const [currentPage, setCurrentPage] = useState("Index");
+
+	//función para mi event onClick
+	const handleGoToGame = () => {
+		setCurrentPage("Game");
+	};
+
+	return (
+		<div>
+			{currentPage === "Index" ? (
+				<Index onGoToGame={handleGoToGame} /> //paso mi handleGoToGame como prop a Index
+			) : (
+				<Game />
+			)}
 		</div>
 	);
 };
