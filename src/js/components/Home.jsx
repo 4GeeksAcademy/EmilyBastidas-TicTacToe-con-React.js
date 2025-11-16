847eb650c01260f780a6387a5eb66cf3a056ca1e
EBastidas <114364709+EmilyBastidas@users.noreply.github.com>
2025-11-16 02:36:29 +0000

    update

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,30 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//componentes
-import Index from './Index';
-import Game from './Game';
+//componente
 
 const Home = () => {
-	// Empezamos mostrando 'Index'
-	const [currentPage, setCurrentPage] = useState("Index");
-
-	//función para mi event onClick
-	const handleGoToGame = () => {
-		setCurrentPage("Game");
-	};
-
-	return (
-		<div>
-			{currentPage === "Index" ? (
-				<Index onGoToGame={handleGoToGame} /> //paso mi handleGoToGame como prop a Index
-			) : (
-				<Game />
-			)}
-		</div>
-	);
 };
 
-export default Home;
+export default Home 
